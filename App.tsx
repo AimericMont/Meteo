bf54c7eda54d3ef1a0ae38f5d7f0a442e61d2153
AimericMont <aimeric.montange@gmail.com>
2021-05-02 17:28:50 +0200

    clear app and use weather component

--- App.tsx
+++ App.tsx
@@ -1,94 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React from 'react';
 
- import React from 'react';
- 
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   TextInput,
-   useColorScheme,
-   View,
-   Button,
-   FlatList,
- } from 'react-native';
+import {View, StyleSheet} from 'react-native';
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+import Weather from './Components/weather';
 
- import meteoData from './Helpers/meteoData'
- import MeteoItem from './Components/meteoItem'
-
- import { getWeatherFromApi } from './API/MDBApi'
-
-
-
- export default class App extends React.Component {
-
-
-  _loadWeather() {
-    getWeatherFromApi().then(data => console.log(data))
-  }
-
- //essaie avec une FlatList, pour comprendre les echanges de données entre component
-   render() {
-     return (
-      <View style={{ marginTop:20}}>
-        <Button title="Rechercher" onPress={()=> getWeatherFromApi()}/>
-        
-       
-        <FlatList
-          data={[
-            {
-                id: 1203,
-                city: "Orgerus",
-                tempMax: 20.1,
-                tempMin: 12.3,
-                main: "clouds",
-                description : "overcast clouds"
-            },
-            {
-                id: 1206,
-                city: "Osmoy",
-                tempMax: 22.1,
-                tempMin: 14.3,
-                main: "sun",
-                description : "hot sun"
-            },
-            {
-                id: 1207,
-                city: "Behoust",
-                tempMax: 24.1,
-                tempMin: 17.3,
-                main: "sun",
-                description : "hot sun"
-            }
-        ]}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) => <MeteoItem meteo={item}/>}
-          />
-
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.screenContainer}>
+        <Weather />
       </View>
-           
-     );
-   }
- }
+    );
+  }
+}
 
-
-
- 
+const styles = StyleSheet.create({
+  screenContainer: {
+    marginTop: 20,
+  },
+});
