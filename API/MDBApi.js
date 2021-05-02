4be8260c66cf0c4c310e9fa7582010c10a326987
AimericMont <aimeric.montange@gmail.com>
2021-05-02 17:26:47 +0200

    stabled api return

--- API/MDBApi.js
+++ API/MDBApi.js
@@ -1,9 +1,8 @@
-const API_TOKEN = "ce89640df774b3137289687a001b811e"
-
-export function getWeatherFromApi(){
-    const url ='pro.openweathermap.org/data/2.5/forecast/hourly?q=London&appid=' + API_TOKEN
-    //const url ='pro.openweathermap.org/data/2.5/forecast/hourly?q=London,us&mode=json&appid=' + API_TOKEN
-    return fetch(url)
-        .then((response) => response.json())
-        .catch((error) => console.log(error))
-}
+export const getWeatherFromApi = () => {
+  //const url ='http://pro.openweathermap.org/data/2.5/forecast?q=London,us&mode=json&appid=ce89640df774b3137289687a001b811e'
+  const url =
+    'http://api.openweathermap.org/data/2.5/forecast?q=Orgerus&appid=ce89640df774b3137289687a001b811e';
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.log(error));
+};
