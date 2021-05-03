9ad022c3867b42c894506ab365b3d12221e52a35
AimericMont <aimeric.montange@gmail.com>
2021-05-03 09:36:02 +0200

    Naviagtion dependancies and start navigation on main app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 
 import {View, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
-import Weather from './Components/weather';
+import Weather from './src/Components/weather';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.screenContainer}>
-        <Weather />
-      </View>
+      <NavigationContainer>
+        <View style={styles.screenContainer}>
+          <Weather />
+        </View>
+      </NavigationContainer>
     );
   }
 }
 
 const styles = StyleSheet.create({
   screenContainer: {
-    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
